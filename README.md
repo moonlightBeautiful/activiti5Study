5a4b952eb58205086663016caf1590825817dda5
agaoxu <xy20539@163.com>
2019-07-19 12:23:26 +0800

    代码层次 跑流程

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # activiti5Study
+1.hello
+    1.1简介：
+        业务流程管理（BPM，也就是工作流）开源项目。
+    1.2推荐书籍：activiti实战
+    1.3代码实战
+        0.用到4个依赖 mysql驱动 activiti引擎和模型  activiti与spring整合
+        1.不使用配置文件初始化activiti
+        2.不使用配置文件初始化activiti
+    1.4activiti自己创建25张表，6种类型
+    1.5安装4activiti辅助插件ActiBMPN
+        Activiti BPMN 2.0 designer
+        http://activiti.org/designer/update/
+        然后选择自动创建流程定义图片当保存实例图的时候
+        NOTE：idea使用的时候有时候报错，从起idea，忽略吧，解决不了
+    1.6跑流程
+        代码层次和数据库层次
+2.
---
+++ src/main/java/com/java1234/c01hello/App.java
@@ -0,0 +1,78 @@
+package com.java1234.c01hello;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.task.Task;
+
+import java.util.List;
+
+/**
+ * @author gaoxu
+ * @date 2019-07-18 10:31
+ * @description ... 类
+ */
+public class App {
+    public static void main(String[] args) {
+
+        // 不使用配置文件生成工作流环境（25张表）
+        /*// 获取流程引擎配置（数据库驱动+地址+用户名+密码）
+        ProcessEngineConfiguration pec = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+        pec.setJdbcDriver("com.mysql.jdbc.Driver");
+        pec.setJdbcUrl("jdbc:mysql://47.244.180.90:3306/db_activiti?useUnicode=true&characterEncoding=UTF-8");
+        pec.setJdbcUsername("root");
+        pec.setJdbcPassword("root");
+
+        // 配置模式：DB_SCHEMA_UPDATE_CREATE_DROP先删除表再创建表， DB_SCHEMA_UPDATE_TRUE如果表不存在，自动创建表，DB_SCHEMA_UPDATE_FALSE不能自动创建表，需要表存在
+        pec.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        // 获取流程引擎对象，并生成25张表
+        ProcessEngine pe = pec.buildProcessEngine();*/
+
+        // 使用配置文件生成工作流环境（25张表）
+        /*
+        // 引擎配置
+        ProcessEngineConfiguration processEngineConfiguration =
+                ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+        // 获取流程引擎对象
+        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();*/
+
+        // 获取默认流程引擎实例，会自动读取activiti.cfg.xml文件
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 部署流程：可以多次部署,貌似使用的是最新的
+        /*
+        // 使用流程引擎（RepositoryService流程仓库服务）
+        Deployment deployment = processEngine.getRepositoryService()
+                .createDeployment()
+                .addClasspathResource("diagrams/Hello.bpmn")
+                .addClasspathResource("diagrams/Hello.png")
+                .name("Hello流程")
+                .deploy();
+        System.out.println("流程部署ID:" + deployment.getId());
+        System.out.println("流程部署Name:" + deployment.getName());*/
+
+        // 启动流程
+        /*// 使用流程引擎（RuntimeService流程运行服务）
+        ProcessInstance processInstance = processEngine.getRuntimeService()
+                .startProcessInstanceByKey("MyFirstProcess");
+        System.out.println("流程实例ID:" + processInstance.getId());
+        System.out.println("流程定义ID:" + processInstance.getProcessDefinitionId());*/
+
+        // 查看任务
+        // 使用流程引擎（TaskService流程任务服务）
+        List<Task> taskList = processEngine.getTaskService()
+                .createTaskQuery()
+                .taskAssignee("高旭")
+                .list();
+        for (Task task : taskList) {
+            System.out.println("任务ID:" + task.getId());
+            System.out.println("任务名称:" + task.getName());
+            System.out.println("任务创建时间:" + task.getCreateTime());
+            System.out.println("任务委派人:" + task.getAssignee());
+            System.out.println("流程实例ID:" + task.getProcessInstanceId());
+        }
+        // 结束任务，如果任务不存在，则跑出异常
+        // 使用流程引擎（TaskService流程任务服务）
+        processEngine.getTaskService()
+                .complete("7504");
+    }
+}
