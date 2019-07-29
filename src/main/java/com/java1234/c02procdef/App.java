9f0005cb594bf701286965ff4a2d693c28774925
agaoxu <xy20539@163.com>
2019-07-29 18:04:32 +0800

    关于流程定义的操作：查询，删除，修改（不能指定流程定义修改，只能升版本），部署

--- src/main/java/com/java1234/c02procdef/App.java
+++ src/main/java/com/java1234/c02procdef/App.java
@@ -4,6 +4,7 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.ProcessDefinition;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -12,7 +13,7 @@ import java.util.List;
  * @description ... 类
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 获取默认流程引擎实例，会自动读取activiti.cfg.xml文件
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         //RepositoryService流程仓库服务：可以多次部署,升版本。部署完之后，在流程定义表中出现流程模板
@@ -38,8 +39,8 @@ public class App {
         System.out.println("流程部署ID:" + deployment.getId());
         System.out.println("流程部署Name:" + deployment.getName());*/
 
-        //查询流程定义：查询Key的所有版本的流程定义，返回流程定义集合，对应表 act_re_procdef
-        String processDefinitionKey = "MyFirstProcess";
+        //查询流程定义：查询Key（流程设计图的id）的所有版本的流程定义，返回流程定义集合，对应表 act_re_procdef
+        /*String processDefinitionKey = "MyFirstProcess";
         List<ProcessDefinition> pdList = processEngine.getRepositoryService() // 获取service
                 .createProcessDefinitionQuery() // 创建流程定义查询
                 .processDefinitionKey(processDefinitionKey) // 通过key查询
@@ -50,7 +51,7 @@ public class App {
             System.out.println("KEY_" + pd.getKey());
             System.out.println("VERSION_" + pd.getVersion());
             System.out.println("=========");
-        }
+        }*/
 
         //查询流程定义：查询ID的某个流程定义,返回流程定义，对应表 act_re_procdef
         /*String processDefinitionId = "MyFirstProcess:2:7504";
@@ -62,5 +63,39 @@ public class App {
         System.out.println("NAME_" + pd.getName());
         System.out.println("KEY_" + pd.getKey());
         System.out.println("VERSION_" + pd.getVersion());*/
+
+        //根据流程部署id和资源文件名称来查询流程图片
+        /*InputStream inputStream = processEngine.getRepositoryService() // 获取sevice
+                .getResourceAsStream("10001", "diagrams/Hello.png");
+        FileUtils.copyInputStreamToFile(inputStream, new File("c:/Hello.png"));*/
+
+        // 查询最新版本的流程定义
+        /*List<ProcessDefinition> listAll = processEngine.getRepositoryService() // 获取service
+                .createProcessDefinitionQuery() // 创建流程定义查询
+                .orderByProcessDefinitionVersion().asc() // 根据流程定义版本升序
+                .list();  // 返回一个集合
+        Map<String, ProcessDefinition> map = new LinkedHashMap<String, ProcessDefinition>();
+        for (ProcessDefinition pd : listAll) {
+            map.put(pd.getKey(), pd);
+        }
+        List<ProcessDefinition> pdList = new LinkedList<ProcessDefinition>(map.values());
+        for (ProcessDefinition pd : pdList) {
+            System.out.println("ID_" + pd.getId());
+            System.out.println("NAME_" + pd.getName());
+            System.out.println("KEY_" + pd.getKey());
+            System.out.println("VERSION_" + pd.getVersion());
+            System.out.println("=========");
+        }*/
+
+        // 删除所有key相同的流程定义
+        String processDefinitionKey = "helloWorld2";
+        List<ProcessDefinition> pdList = processEngine.getRepositoryService() // 获取service
+                .createProcessDefinitionQuery() // 创建流程定义查询
+                .processDefinitionKey(processDefinitionKey) // 根据key查询
+                .list();  // 返回一个集合
+        for (ProcessDefinition pd : pdList) {
+            processEngine.getRepositoryService()
+                    .deleteDeployment(pd.getDeploymentId(), true);
+        }
     }
 }
--- src/main/java/com/java1234/c02procdef/README.md
+++ src/main/java/com/java1234/c02procdef/README.md
@@ -1,11 +1,26 @@
 # activiti5Study procdef
-   流程定义部署：流程图的id不变，重复部署，则升级版本
+   流程定义部署：
+      流程图的指定的流程id不变，重复部署，部署id会变和流程定义id不变和流程版本会升级
         bmpn+png 方式
         bmpn+png的zip 方式
-   流程定义查询
+   流程定义查询：ACT_RE_PROCDEF
         查询Key版本的所有流程定义
         查询指定id的流程定义
-        
+        查询流程定义的设计图片：资源表ACT_GE_BYTEARRAY
+            ACT_RE_PROCDEF：部署id和资源名称
+            ACT_GE_BYTEARRAY：部署id和名称（资源名称）和bytes(资源存放在这里)
+            用到commons-io插件
+        查询最新版本的全部流程定义：同id下流程的最新版本
+            1.查询流程定义，按照版本升序
+            2.使用Map筛选掉旧的流程版本：key是流程定义id，不选最新覆盖旧的。
+   流程定义删除：
+        删除key（即流程bmpn图中的id）相同的所有流程定义
+            1.查询指定key的所有的流程定义
+            2.然后再根据流程定义的部署id，级联删除true（会把流程部署和流程定义和流程实例都删除）
+            级联影响到了：流程部署表（ACT_RE_DEPLOYMENT）、流程定义表（ACT_RE_PROCDEF）其他相关的表（实例、资源）中数据都删除了
+        也可以删除某一个版本的，查找到指定版本的流程的流程部署id
+   流程定义修改：
+        不允许直接修改指定版本，可以重新部署，升版本。
    note:
         向数据库传递数据，中文乱码，解决：
             数据库地址后+?useUnicode=true&amp;characterEncoding=UTF-8
